6544a3bc07486a9ba986270169a75347ea230dbd
aledcuevas <aledancuevas@gmail.com>
2025-05-05 08:46:00 -0400

    updates

--- walkthrough/solver.py
+++ walkthrough/solver.py
@@ -7,10 +7,10 @@ full_encoded_message = "FIXME"
 # Step 1: First Base64 decoding
 decoded_once = base64.b64decode(FIXME)
 
-# Step 3: Determine the script's directory
+# Step 2: Determine the script's directory
 image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "decoded_image.png")  # Creates the full path for the image
 
-# Step 4: Save as an image
+# Step 3: Save as an image
 with open(image_path, "wb") as image_file:
     image_file.write(decoded_once)
 
